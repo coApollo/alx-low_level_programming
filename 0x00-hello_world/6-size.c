3d13df887d04371f80b6bcf2b179437a127bd357
coApollo <bristarcollins@gmail.com>
2023-07-13 15:24:44 -0700

    101-quote.c file commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
-int maain(void)
+int main(void)
 {
 	printf("size of a char: %d byte(s)\n", sizeof(char));
 	printf("size of an int: %d byte(s)\n", sizeof(int));
