f4a460f7994bb67c93e51e52d59fc663d2a7558d
coApollo <bristrcollins@gmail.com>
2023-07-15 01:04:45 +0300

    printig hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,7 +7,17 @@
  */
 int main(void)
 {
-	
+	int d;
+	char C;
+
+	for (d = '0' ; d <= '9' ; d++)
+		putchar(d);
 
 
+	for (C = 'a' ; C <= 'f' ; C++)
+		putchar(C);
+
+	putchar('\n');
+
+	return (0);
 }
