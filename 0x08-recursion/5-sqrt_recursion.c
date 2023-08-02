02299935ecab29ac8298700cabd22d4ce4b97774
coApollo <bristrcollins@gmail.com>
2023-08-03 01:16:56 +0300

    update

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 #include <math.h>
 #include "main.h"
+
+int actual_sqrt_root(int n, int i);
 /**
- *main - Entry point
+ *_sqrt_recursion - find squareroot of a number
  *
+ *@n: number
  *Return: Always 0 (Success)
  */
-int _sqrt_recursion(int n, int i)
+int _sqrt_recursion(int n)
 {
 
 	if (n < 0)
 		return (-1);
 
 	return (actual_sqrt_root(n, 0));
-	
+
 }
 
 /**
- *main - Entry point
+ *actual_sqrt_root - function to find squareroot
  *
+ *@n:number
+ *@i:square root
  *Return: Always 0 (Success)
  */
 int actual_sqrt_root(int n, int i)
@@ -29,5 +34,5 @@ int actual_sqrt_root(int n, int i)
 	if (i * i == n)
 		return (i);
 	return (actual_sqrt_root(n, i + 1));
-	
+
 }
