ad21aa82811f7a7f20057ec27cffa7843babcbeb
coApollo <bristrcollins@gmail.com>
2023-08-25 01:39:55 +0300

    some corrections on file 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,13 +10,19 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	char *dup;
 	list_t *new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->str = strdup(str);
-
+	dup = strdup(str);
+	if (dup == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->str = dup;
 	new_node->next = *head;
 
 	*head = new_node;
