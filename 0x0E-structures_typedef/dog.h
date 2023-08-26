a3582c7318623bbb530afb97aeb2bb1cf3588bce
coApollo <bristrcollins@gmail.com>
2023-08-26 23:45:25 +0300

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,10 +11,6 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-};
-/*
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-*/
+}my_dog;
 
 #endif
